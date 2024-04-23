086f64f3a5c77df39f97486521550d98cae70e3c
Amos <amos.weckstrom@abo.fi>
2024-04-23 19:14:40 +0200

    added buttons

--- Frontend/DCR_Frontend/src/GraphElements/GraphObject.jsx
+++ Frontend/DCR_Frontend/src/GraphElements/GraphObject.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import {Rect, Text, Group, Layer} from "react-konva";
-import Event from './Event';
-import Relation from './Relation';
+import { Rect, Text, Group, Layer } from "react-konva";
+import Event from "./Event";
+import Relation from "./Relation";
 
 export default class GraphObject extends React.Component {
-    constructor(props) {
-        super(props);
-        this.events = [];
-        this.relations = [];
-    }
-    addEvent(pos, size, label) {
-        this.events.push(<Event pos={pos} size={size} label={label}/>);
-    }
-    addRelation(fromEvent, toEvent, label) {
-        this.relations.push(<Relation fromEvent={fromEvent} toEvent={toEvent} label={label}/>);
-    }
-    handleClick = () => {
-        console.log("GraphObject clicked");
-    }
-    render() {
-        return (
-            <Layer>
-                {this.events}
-                {this.relations}
-            </Layer>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.events = [];
+    this.relations = [];
+  }
+  addEvent(pos, size, label) {
+    this.events.push(<Event pos={pos} size={size} label={label} />);
+  }
+  addRelation(fromEvent, toEvent, label) {
+    this.relations.push(
+      <Relation fromEvent={fromEvent} toEvent={toEvent} label={label} />
+    );
+  }
+  handleClick = () => {
+    console.log("GraphObject clicked");
+  };
+  render() {
+    return (
+      <Layer>
+        {this.events}
+        {this.relations}
+      </Layer>
+    );
+  }
 }
--- Frontend/DCR_Frontend/src/GraphElements/Relation.jsx
+++ Frontend/DCR_Frontend/src/GraphElements/Relation.jsx
@@ -1,28 +1,33 @@
 import React from "react";
-import {Arrow, Text, Group} from "react-konva";
+import { Arrow, Text, Group } from "react-konva";
 
 class Relation extends React.Component {
   constructor(props) {
     super(props);
-    this.id = props.relationId
-    this.fromPos = props.fromEvent.props.pos
-    this.toPos = props.toEvent.props.pos
-    this.fromWidth = props.fromEvent.props.size[0]
-    this.toWidth = props.toEvent.props.size[0]
-    this.fromHeight = props.fromEvent.props.size[1]
-    this.toHeight = props.toEvent.props.size[1]
-    this.points = [this.fromPos[0] + this.fromWidth, this.fromPos[1] + this.fromHeight / 2, this.toPos[0], this.toPos[1] + this.toHeight / 2]
+    this.id = props.relationId;
+    this.fromPos = props.fromEvent.props.pos;
+    this.toPos = props.toEvent.props.pos;
+    this.fromWidth = props.fromEvent.props.size[0];
+    this.toWidth = props.toEvent.props.size[0];
+    this.fromHeight = props.fromEvent.props.size[1];
+    this.toHeight = props.toEvent.props.size[1];
+    this.points = [
+      this.fromPos[0] + this.fromWidth,
+      this.fromPos[1] + this.fromHeight / 2,
+      this.toPos[0],
+      this.toPos[1] + this.toHeight / 2,
+    ];
     if (this.fromPos[0] > this.toPos[0]) {
-      this.points[2] = this.toPos[0] + this.toWidth
-      this.points[3] = this.toPos[1] + this.toHeight / 2
-      this.points[0] = this.fromPos[0]
+      this.points[2] = this.toPos[0] + this.toWidth;
+      this.points[3] = this.toPos[1] + this.toHeight / 2;
+      this.points[0] = this.fromPos[0];
     }
     if (this.fromPos[1] > this.toPos[1]) {
-      this.points[2] = this.toPos[0] + this.toWidth / 2
-      this.points[3] = this.toPos[1] + this.toHeight
-      this.points[1] = this.fromPos[1]
+      this.points[2] = this.toPos[0] + this.toWidth / 2;
+      this.points[3] = this.toPos[1] + this.toHeight;
+      this.points[1] = this.fromPos[1];
     }
-    this.label = props.label
+    this.label = props.label;
   }
   state = {
     color: "red",
@@ -34,29 +39,25 @@ class Relation extends React.Component {
   };
   render() {
     return (
-      <Group
-      x= {0}
-      y= {0}
-      onDblClick={this.handleClick}
-    >
-      <Arrow
-        points= {this.points}
-        fill={this.state.color}
-        stroke={"black"}
-        shadowBlur={10}
-        onClick={this.handleClick}
-      />
-      <Text
-        text={this.label}
-        fontSize={15}
-        fill="white"
-        x={0}
-        y={this.height / 2 - 10}
-        width={this.width}
-        height={this.height}
-        align="center"
-      />
-    </Group>
+      <Group x={0} y={0} onDblClick={this.handleClick}>
+        <Arrow
+          points={this.points}
+          fill={this.state.color}
+          stroke={"black"}
+          shadowBlur={10}
+          onClick={this.handleClick}
+        />
+        <Text
+          text={this.label}
+          fontSize={15}
+          fill="white"
+          x={0}
+          y={this.height / 2 - 10}
+          width={this.width}
+          height={this.height}
+          align="center"
+        />
+      </Group>
     );
   }
 }
