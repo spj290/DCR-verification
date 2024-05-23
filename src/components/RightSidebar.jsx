5414c4b751f062bc0a0c433488474195d52cb552
spj290 <79872732+spj290@users.noreply.github.com>
2024-05-23 15:21:01 +0200

    Fixed delete bug. Sub-optimal

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -48,15 +48,15 @@ function RightSidebar({ selectedEventId, events, updateEventLabel, setEvents }) 
       </form>
       <ul className="event-properties">
         <li><input type="checkbox"
-                   checked={events.find((event) => event.id === selectedEventId).pending}
+                   checked={events.find((event) => event.id === selectedEventId) ? events.find((event) => event.id === selectedEventId).pending : false}
                    onChange={handlePending}
                    />Pending</li>
         <li><input type="checkbox"
-                  checked={events.find((event) => event.id === selectedEventId).included}
+                  checked={events.find((event) => event.id === selectedEventId) ? events.find((event) => event.id === selectedEventId).included : false}
                   onChange={handleIncluded}
                   />Included</li>
         <li><input type="checkbox"
-                  checked={events.find((event) => event.id === selectedEventId).executed}
+                  checked={events.find((event) => event.id === selectedEventId) ? events.find((event) => event.id === selectedEventId).executed : false}
                   onChange={handleExecuted}
                   />Executed</li>
       </ul>
